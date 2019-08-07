bea6f5a018616c853cd4596402cc9f7a1f000327
NeherHalder <neher1994@gmail.com>
2019-08-07 22:26:44 +0600

    initial commit

---
+++ app/src/main/java/com/rannaghor/rannaghor/Env.java
@@ -0,0 +1,11 @@
+package com.rannaghor.rannaghor;
+
+public class Env {
+
+    public static final class app{
+        public static final String name = "Ranna Ghor";
+        public static final String mobile = "";
+       
+
+    }
+}
